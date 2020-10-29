2065812b4b2e48cc2048ea1e1593d091927820c2
unknown <xiongjoh@gmail.com>
2020-10-29 15:17:39 -0500

    add id to link label to forms

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent, act } from '@testing-library/react'
 import ContactForm from './ContactForm'
 
 test('Renders ContactForm without errors', () => {
@@ -15,5 +15,21 @@ test('Check form inputs and submit button', () => {
     const message = screen.getByLabelText(/message/i);
     const submit = screen.getByRole("button");
 
-    fireEvent.change(firstname, {target: {value: 'Edd', name:'firstName'}})
+    act(() => {
+        fireEvent.change(firstname, {target: {value: 'Edd', name:'firstName'}})
+        expect(firstname).toHaveValue('Edd')
+
+        fireEvent.change(lastname, {target: {value:'Burke', name:'lastName'}})
+        expect(lastname).toHaveValue('Burke')
+
+        fireEvent.change(email, {target: {value:'123@gmail.com', name:'email'}})
+        expect(email).toHaveValue('123@gmail.com')
+
+        fireEvent.change(message, {target: {value:'Hello', name:'message'}})
+        expect(message).toHaveValue('Hello')
+    })
+    // act(() => {
+    //     fireEvent.click(submit)
+    //     expect
+    // })
 } )
